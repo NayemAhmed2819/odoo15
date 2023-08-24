b808554d49478d9a4fd59891b30451849aece3a3
NayeemAhmed <nayeemahmed2819@gmail.com>
2023-08-24 14:33:31 +0600

    first commit

---
+++ custom_addons/odoo_inheritence/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "odoo_inheritence",
+    'summary': """""",
+    'description': """""",
+    'author': "Odoo Learner",
+    'website': "",
+    'version': '0.1',
+    'depends': ['sale','mail'],
+    'data': [
+        'views/sale_order_view.xml',
+        'views/account_move_view.xml',
+        'views/partner_category_view.xml',
+    ],
+    'demo': [],
+}
---
+++ custom_addons/odoo_inheritence/models/res_partner_category.py
@@ -0,0 +1,9 @@
+from odoo import api, fields, models
+
+
+class PartnerCategory(models.Model):
+    _name = 'res.partner.category'
+    _inherit = ['res.partner.category', 'mail.thread']
+
+
+
